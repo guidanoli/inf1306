c5f1a0b8b095587e14db9e639d90aedeabca56e2
Guilherme Dantas <guidanoli@hotmail.com>
2019-09-10 08:13:57 -0300

    Fixed E2D distance calculation
    * delta x and delta y can be negative, thus have to be stored in signed integers
    * return unsigned int, and not floating points

--- Project-01/src/utils.c
+++ Project-01/src/utils.c
@@ -1,5 +1,5 @@
 /*
- * utils.c, v.0.1.0
+ * utils.c, v.0.1.1
  *
  * Utility functions for GVRP
  */
@@ -9,9 +9,9 @@
 
 unsigned int e2d_dist(struct node_t *a, struct node_t *b)
 {
-        unsigned int dx = a->x - b->x;
-        unsigned int dy = a->y - b->y;
+        int dx = a->x - b->x;
+        int dy = a->y - b->y;
         float dist = sqrt( (float)(dx * dx + dy * dy) );
         unsigned int idist = (unsigned int) dist;
-        return (dist - idist) < 0.5f ? dist : dist + 1;
+        return (dist - idist) < 0.5f ? idist : idist + 1;
 }
