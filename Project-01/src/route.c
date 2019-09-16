1169d07fbbcf506027938c56b16faba349570319
guidanoli <guidanoli@hotmail.com>
2019-09-16 18:51:01 -0300

    Route module basics
    * Added create_route
    * Added free_route
    * Added add_customer
    * Added remove_customer
    * Added basic test to add and remove N customers based on instance

--- Project-01/src/route.c
+++ Project-01/src/route.c
@@ -1,9 +1,73 @@
 /*
- * route.c, v.0.1.0
+ * route.c, v.0.2.0
  * 
  * GVRP route
  */
 
+#include <stdlib.h>
 #include "route.h"
 
+struct route_t *create_route()
+{
+	struct route_t *route = malloc(sizeof(struct route_t));
+	if (!route)
+		return NULL;
+	
+	if (lwct_sll_create(&(route->list)) != LWCTL_SLL_OK) {
+		free_route(route);
+		return NULL;
+	}
+	
+	return route;
+}
 
+void free_route(struct route_t *route)
+{
+	if (!route) return;
+	
+	if (route->list)
+		lwct_sll_destroy(route->list);
+	
+	free(route);
+}
+
+route_ret add_customer(struct route_t *route, struct customer_t *customer)
+{
+	if (!route || !customer)
+		return ROUTE_PARAM;
+	
+	switch (lwct_sll_insert(route->list, customer, NULL)) {
+		case LWCTL_SLL_OK:
+			return ROUTE_OK;
+		case LWCTL_SLL_MEM:
+			return ROUTE_MEM;
+		default:
+			return ROUTE_UNEXPECTED;
+	}
+}
+
+route_ret remove_customer(struct route_t *route, struct customer_t *customer)
+{
+	if (!route || !customer)
+		return ROUTE_PARAM;
+	
+	/* Searches for customer in route and, if found, makes it current */
+	switch(lwct_sll_search(route->list, customer)) {
+		case LWCTL_SLL_CONTAINS:
+			break;
+		case LWCTL_SLL_DOES_NOT_CONTAIN:
+			return ROUTE_CUSTOMER_NOT_FOUND;
+		default:
+			return ROUTE_UNEXPECTED;
+	}
+	
+	/* The customer pointer will be overriden but only locally */
+	switch (lwct_sll_remove(route->list, &customer)) {
+		case LWCTL_SLL_OK:
+			return ROUTE_OK;
+		case LWCTL_SLL_MEM:
+			return ROUTE_MEM;
+		default:
+			return ROUTE_UNEXPECTED;
+	}
+}
--- Project-01/tests/t.route.c
+++ Project-01/tests/t.route.c
@@ -9,14 +9,37 @@
 #include "route.h"
 #include "lwct.h"
 
+#define NULL ((void *) 0)
+
+void testNull(lwct_state *S)
+{
+	free_route(NULL);
+	lwct_assert(S, 1); // should not halt
+}
+
 void testRoute(lwct_state *S)
 {
 	struct instance_t *instance = parse_gvrp_instance();
 	lwct_submit_desconstructor(S, free_gvrp_instance, instance);
 	lwct_fatal_assert(S, instance);
+	
+	struct route_t *route = create_route();
+	lwct_submit_desconstructor(S, free_route, route);
+	lwct_fatal_assert(S, route);
+	
+	for (int i = 0; i < instance->customer_cnt; i++) {
+		route_ret ret = add_customer(route, &(instance->customers[i]));
+		lwct_assert(S, ret == ROUTE_OK);
+	}
+	
+	for (int i = 0; i < instance->customer_cnt; i++) {
+		route_ret ret = remove_customer(route, &(instance->customers[i]));
+		lwct_assert(S, ret == ROUTE_OK);
+	}
 }
 
 int main(void)
 {
+	lwct_submit_test(testNull);
 	lwct_submit_test(testRoute);
 }
